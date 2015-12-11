7dce1f884fd824fd14a416996df2d2548410b835
Hesham Massoud <hesham.massoud@commercetools.de>
2015-12-11 15:26:43 +0100

    Add view controller logic for tip calculator

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPercantage: 0.06)
 
+    @IBOutlet weak var totalBillTextField: UITextField!
+    @IBOutlet weak var taxPercentageSlider: UISlider!
+    @IBOutlet weak var taxPercentageLabel: UILabel!
+    @IBOutlet weak var resultsTextView: UITextView!
+    
+    @IBAction func calculateButtonTapped(sender: AnyObject) {
+        tipCalc.total = Double((totalBillTextField.text! as NSString).doubleValue)
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        for (tipPercentage, tipValue) in possibleTips {
+            results += "\(tipPercentage)%: \(tipValue)\n"
+        }
+        resultsTextView.text = results
+    }
+    
+    @IBAction func taxPercentageChanged(sender: AnyObject) {
+        tipCalc.taxPercantage = Double(taxPercentageSlider.value) / 100.0
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender: AnyObject) {
+        totalBillTextField.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func refreshUI() {
+        totalBillTextField.text = String(tipCalc.total)
+        taxPercentageSlider.value = Float(tipCalc.taxPercantage) * 100.0
+        taxPercentageLabel.text = "Tax Percentage (\(Int(taxPercentageSlider.value))%)"
+        resultsTextView.text = ""
+    }
 
 }
 
